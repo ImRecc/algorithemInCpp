8e73b8f472ca56229ac6c9ec754aa4fe1a32d8df
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-02 06:45:57 -0400

    canPlaceFlower?

---
+++ commonDataDealing/canPlaceFlower.c
@@ -0,0 +1,26 @@
+//1.leave edge along
+//2.find out all triple 0 and counter +=1
+//3.check out if brink are double 0 if so +=1
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        //we created a new bed which embody brink as 0
+        int rawSize = flowerbed.size(), bed1Size=rawSize+2;
+        int bed1[bed1Size]; 
+        bed1[0]=0, bed1[bed1Size-1]=0;
+        for (int i=0; i<rawSize; i++) bed1[i+1]=flowerbed[i];
+        
+        //once triple 0 found, we insert a flower and counter += 1;
+        int counter=0;
+        for (int i=1; i<bed1Size-1; i++)
+        {
+            if(bed1[i-1]==0 && bed1[i]==0 && bed1[i+1]==0)  
+            //dunnon why 1==2==3 failed, language feature..
+            {                                       
+                counter++;
+                bed1[i]=1;
+            }
+        }
+        return (counter >= n);
+    }
+};
