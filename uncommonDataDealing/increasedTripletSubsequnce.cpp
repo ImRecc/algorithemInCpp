9332cb4ff3f406a4d1e239017a4654b28de23259
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-03 09:37:27 -0400

    Add files via upload

---
+++ uncommonDataDealing/increasedTripletSubsequnce.cpp
@@ -0,0 +1,29 @@
+//interesting pazzle
+
+//no matter how the smallest element change
+//we need one second small element shows up
+//!! the second one are marks as a symbol of someone smaller than it
+
+//same, no matter how first or second one change, the order are exist and detected
+
+//when third spotted, we are done;
+
+class Solution {
+ public:
+  bool increasingTriplet(vector<int>& nums) {
+    int first = INT_MAX;        // INT_MAX or INT_MIN
+    int second = INT_MAX;       //like they literal
+
+    for (const int num : nums)  //so proger cannot modifie
+      if (num <= first)
+        first = num;
+      else if (num <= second)  // once shows up, the order detected
+        second = num;          //can record like first.now<second.now
+      else
+        return true;  // once shows up, the order detected too
+                      //but the sequnce? have to record when second changed  
+
+    return false;
+  }
+};
+
---
+++ uncommonDataDealing/readme.md
@@ -0,0 +1,3 @@
+#interesting only!
+>some progg that hard but boring are not permit!
+#progs that makes me "wow" or opened wide sight ONLY
