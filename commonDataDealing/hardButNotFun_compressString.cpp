db762d333219103b8b0e5a1d9c496b035d922434
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-04 02:51:21 -0400

    Create hardButNotFun_compressString.cpp

---
+++ commonDataDealing/hardButNotFun_compressString.cpp
@@ -0,0 +1,19 @@
+//just a double pointer, not fun at all
+class Solution{
+public:
+int compress(vector<char>& chars) {
+	int recorder = 0;
+	for(int readPoint = 1, count = 1; readPoint <= chars.size(); readPoint++, count++) 
+    {
+		if(readPoint == chars.size() || chars[readPoint] != chars[readPoint - 1]) 
+        {
+			chars[recorder++] = chars[readPoint - 1];       //note that char[r]=char[rp-1] r++  
+			if(count >= 2) 
+				for(char digit : to_string(count)) 
+					chars[recorder++] = digit;
+			count = 0;
+		}
+	}
+	return recorder;
+    }
+};
