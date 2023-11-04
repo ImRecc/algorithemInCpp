f404919b1044684cbb5369b1c1500498fd4cec4a
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-04 07:26:34 -0400

    understanding hardButNotFun_compressString.cpp

--- commonDataDealing/hardButNotFun_compressString.cpp
+++ commonDataDealing/hardButNotFun_compressString.cpp
@@ -4,11 +4,11 @@ public:
 int compress(vector<char>& chars) {
 	int recorder = 0;
 	for(int readPoint = 1, count = 1; readPoint <= chars.size(); readPoint++, count++) 
-    {
+    	{
 	    //note that recorder only use for record, and reader only compare with itself's past
 	    //there are seperated
 		if(readPoint == chars.size() || chars[readPoint] != chars[readPoint - 1]) 
-        {
+        	{
 			chars[recorder++] = chars[readPoint - 1];       //note that char[r]=char[rp-1] r++  
 			if(count >= 2) 
 				for(char digit : to_string(count)) 
@@ -16,6 +16,7 @@ int compress(vector<char>& chars) {
 			count = 0;
 		}
 	}
-	return recorder;
+	return recorder;		//the recorder also play as "the last one standed"
+	//we don't do any clearance 'cause only display and exam the first(recorder) number of the vector;
     }
 };
