923ed343428d8804543ef368743f7732d4574923
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-10 22:51:56 -0500

    Update 1456. Maximum Number of Vowels in a Substring of Given Length.cpp

--- uncommonDataDealing/slideWindowType/1456. Maximum Number of Vowels in a Substring of Given Length.cpp
+++ uncommonDataDealing/slideWindowType/1456. Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -6,7 +6,8 @@ public:
     unordered_set<char> vowelsSet = {'a', 'e', 'i', 'o', 'u'};
     int upPointer = 0, lowPointer = 0;
     int maxVowels = 0, currentVowels = 0;
-    while (lowPointer < s.length()) 
+    int length=s.length();
+    while (lowPointer < length) 
     {
         if (vowelsSet.count(s[lowPointer]) > 0) //a set like <key, equal2Key>
         {
@@ -15,14 +16,14 @@ public:
 
         if (lowPointer - upPointer + 1 > k) //like 3->1 are 3long but 3-1=2 so plus1
         {
-            if (vowelsSet.count(s[upPointer]) > 0) 
+            if (vowelsSet.count(s[upPointer]) > 0)  //when slide oversize, if counted, then substrac
             {
                 currentVowels--;
             }
-            upPointer++;
+            upPointer++;            //move begin of slider
         }
         //smart way to iterate every k'long array on a slide
-        //each time, check if *upPointer==vow, are reducing the overCount from previous3
+        //each time, check if *upPointer==vow, are reducing the overCount from previousK'slider
         //and slide beginPoint smart way
 
         maxVowels = max(maxVowels, currentVowels);
