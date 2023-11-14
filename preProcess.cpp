54e21b21e36196a9b1ba6f0e88d7fc4c0b232288
ImRecc <46367069+ImRecc@users.noreply.github.com>
2023-11-14 06:28:12 -0500

    Create preProcess.cpp
    to record what submitted but not done yet and need to deep dive in...

---
+++ preProcess.cpp
@@ -0,0 +1,8 @@
+1207. unique number of occurrences
+724. find pivot index
+1732. find the highest altitude
+1493. longest subarray of 1's after deleting one element
+933. Number of recent calls
+206. reverse linked list
+104. maximum depth of binary tree
+872. leaf-similar trees
